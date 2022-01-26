2ccf423ad397fa1c35f03062649f3a36215f0c73
sweet-boyw <wangxj848@qq.com>
2022-01-26 09:29:17 +0800

    删除main分支demo，新建myapp项目

---
+++ myapp/src/router/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-26 09:19:31
+ * @LastEditTime: 2022-01-26 09:28:20
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue-demo\myapp\src\router\index.js
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router

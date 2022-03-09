0d6d75bd4dc828f4406eb8b348970cd1a23bd89c
sweet-boyw <wangxj848@qq.com>
2022-03-09 08:00:53 +0800

    index接口测试

--- managesystem/src/api/api.js
+++ managesystem/src/api/api.js
@@ -1,13 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2022-03-03 20:58:47
- * @LastEditTime: 2022-03-03 21:01:20
+ * @LastEditTime: 2022-03-08 15:18:23
  * @LastEditors: Please set LastEditors
  * @Description: 接口封装
  * @FilePath: \demospace\vue-demo\managesystem\src\api\api.js
  */
 import request from '@/utile/request'
-const BaseUrl = ''
+const BaseUrl = 'http://192.168.101.28:5000/'
 
 export function getCode(){
     return request({

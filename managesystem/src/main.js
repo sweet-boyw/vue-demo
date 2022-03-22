33783a4e31d1407a35a9e69a538c7f6cd478acf8
sweet-boyw <wangxj848@qq.com>
2022-03-22 17:08:57 +0800

    公共方法封装引入

--- managesystem/src/main.js
+++ managesystem/src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-14 10:36:41
- * @LastEditTime: 2022-03-21 16:46:13
+ * @LastEditTime: 2022-03-22 16:58:20
  * @LastEditors: Please set LastEditors
  * @Description: 项目入口配置、组件引入
  * @FilePath: \vue-demo\managesystem\src\main.js
@@ -14,6 +14,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import echart from 'echarts'
 import '@/api/http.js'
+import '@/utile/utile.js'
 
 import '@/style/style.scss'
 
---
+++ managesystem/src/utile/utile.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-22 16:53:34
+ * @LastEditTime: 2022-03-22 17:07:19
+ * @LastEditors: Please set LastEditors
+ * @Description: 这里封装公共方法
+ * @FilePath: \vue-demo\managesystem\src\utile\utile.js
+ */
+const utile = {
+    handleLog(){
+        console.log('aaaaaaaaaaaaaaa')
+    },
+    // 你要获取哪天时间-1表示昨天,默认传入0表示今天时间
+    handleGetDate(day = 0){
+        const date = new Date()
+        date.setTime(date.getTime() + day*24*60*60*1000)
+        const YY = date.getFullYear()
+        const MM = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+        const DD = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+        const hh = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+        const mm = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+        const ss = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+        return `${YY}-${MM}-${DD} ${hh}:${mm}:${ss}`
+    }
+}
+
+import Vue from 'vue'
+Vue.prototype.$utile = utile
